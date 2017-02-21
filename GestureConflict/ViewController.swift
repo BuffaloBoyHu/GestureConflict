ed88f25efa4407bc47cc4835fd62da9c99864b01
buffalohlh <hulianghai@xmzhen.com>
2017-02-21 18:03:15 +0800

    手势冲突

---
+++ GestureConflict/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GestureConflict
+//
+//  Created by HLH on 2017/2/21.
+//  Copyright © 2017年 胡良海. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
